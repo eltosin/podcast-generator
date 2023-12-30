46892d172cccc399bd7cb4876cd0f076686e0fea
Tosin Olaolu Oyeniyi <oyeniyitosin27@gmail.com>
2023-12-30 17:39:44 +0000

    Changed Entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "==================="
 
-git config --global user.name "${GITHUB_ACTOTR}"
+git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
